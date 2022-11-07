814510e14555e07147d4a28d1848d735e1fe088d
Dieselist300 <117685432+Dieselist300@users.noreply.github.com>
2022-11-08 00:43:04 +0400

    Add files via upload

---
+++ The_address_book/src/mocks/mocks.js
@@ -0,0 +1,77 @@
+const Mocks = () => {
+    const mocks = [{
+        "id": 1,
+        "name": 'Нико Нико Дога',
+        "phone": '+7(917)658-39-44',
+        "favorite": 'favorite'
+    },
+    {
+        "id": 2,
+        "name": 'Ремонт ДВС Чебоксары',
+        "phone": '+7(827)321-45-69',
+        "favorite": 'none'
+    },
+    {
+        "id": 3,
+        "name": 'Саня Заливной',
+        "phone": '+7(913)178-30-23',
+        "favorite": 'none'
+    },
+    {
+        "id": 4,
+        "name": 'Марина большое горло',
+        "phone": '+7(875)456-78-90',
+        "favorite": 'none'
+    },
+    {
+        "id": 5,
+        "name": 'Джонатан Джостар',
+        "phone": '+7(918)678-79-56',
+        "favorite": 'favorite'
+    },
+    {
+        "id": 6,
+        "name": 'Джозеф Джостар',
+        "phone": '+7(958)178-91-37',
+        "favorite": 'none'
+    },{
+        "id": 7,
+        "name": 'Какёин',
+        "phone": '+7(906)142-34-85',
+        "favorite": 'none'
+    },
+    {
+        "id": 8,
+        "name": 'Пальнареф',
+        "phone": '+7(925)365-90-75',
+        "favorite": 'none'
+    },
+    {
+        "id": 9,
+        "name": 'Коля Рефрежератор',
+        "phone": '+7(928)980-78-34',
+        "favorite": 'none'
+    },
+    {
+        "id": 10,
+        "name": 'Леха Дизель',
+        "phone": '+7(906)158-68-81',
+        "favorite": 'none'
+    },
+    {
+        "id": 11,
+        "name": 'Максон Ровный Чел',
+        "phone": '+7(906)228-36-94',
+        "favorite": 'none'
+    },
+    {
+        "id": 12,
+        "name": 'Алмаз Доставка',
+        "phone": '+7(928)762-62-32',
+        "favorite": 'favorite'
+    }]
+
+    return mocks
+}
+
+export default Mocks
